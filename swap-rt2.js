48c52279e646347ce69cfa8590520bbb22f53813
unknown <alajfit@gmail.com>
2015-07-17 16:43:50 +0100

    Updated a new swapper with a global regex

--- swap-rt2.js
+++ swap-rt2.js
@@ -6,12 +6,8 @@
 			var $cache = $(this);
 			var $allElements = $cache.text().match(/\b([0-9]{2}\/[0-9]{2})\b(?![0-9]|[a-z]|[ ])/g);
 			if($allElements) { // checking elements have been found
-				$.each($allElements, function( key, value ) {
-						var $swapOut = $allElements[key];
-						var $swapIn = $swapOut.substring(0, $swapOut.length - 3);
-						var replaced = $cache.html().replace($swapOut, $swapIn);
-						$cache.html(replaced);
-				});
+				var replaced = $cache.html().replace(/\/([0-9]{2})\b(?![0-9]|[a-z]|[ ])/g, "");
+				$cache.html(replaced);
 			}
 		});
 		console.log("finished");
