3069e1001a06ab74a951a0f1c657d9bc1daef9c3
unknown <alajfit@gmail.com>
2015-07-17 09:20:18 +0100

    New RegEx added

--- auto-rt.js
+++ auto-rt.js
@@ -6,7 +6,7 @@ rtAuto.load = (function(){
 			var countryC = true;
 			switch (rtAuto.co) {
 				case 'es':
-					var $country = new RegExp("([0-9]{2} ?[0-9]{3} ?[0-9]{2} ?[0-9]{2})|([0-9]{2,} ?[0-9]{3,} ?[0-9]{2,})","g");
+					var $country = new RegExp("\b([0-9]{2} ?[0-9]{3} ?[0-9]{2} ?[0-9]{2})|([0-9]{2,} ?[0-9]{3,} ?[0-9]{2,})\b","g");
 					break;
 				default:
 					countryC = false;
